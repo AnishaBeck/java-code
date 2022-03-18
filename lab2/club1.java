43b33cb13ad1731a3bdf3d02138d49d3269c8cb5
AnishaBeck <75293907+AnishaBeck@users.noreply.github.com>
2022-03-18 12:54:18 +0530

    update

--- lab2/club1.java
+++ lab2/club1.java
@@ -36,144 +36,7 @@ public class club1{
         System.out.println("Play position: "+ play_position);
         System.out.println("Contact: "+ p_contact);
     }
-public ResultSet getInfo(){
-        String query;
-        try{
-            query="select * from `"+al.get(0)+"` where username='"+al.get(1)+"' and password='"+al.get(2)+"';";
-            rs=st.executeQuery(query);
-            System.out.println(query);
-            return rs;
-        }
-        catch(Exception e){
-            System.out.println("Error: "+e);
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
-    public void insert(ArrayList ia){
-        try{
-            String query=null;
-            
-            if(ia.get(0)=="player"){
-                 query= "Insert into "+ia.get(0)+" (username,password,name,Jersey_no,gender,phone,email,Address,birth_date,debute,player_role,bat_style,bowl_style,runs,wickets,catch,stumping) values('"
-                    +ia.get(1)+"','"+ia.get(2)+"','"+ia.get(3)+"',"+ia.get(4)+",'"+ia.get(5)+"','"+ia.get(6)+"','"+ia.get(7)
-                    +"','"+ia.get(8)+"','"+ia.get(9)+"',CURRENT_DATE,'"+ia.get(11)+"','"+ia.get(12)
-                         +"','"+ia.get(13)+"',"+ia.get(14)+","+ia.get(15)+","+ia.get(16)+","+ia.get(17)+");";
-            }
-            else if(ia.get(0)=="coach"){
-                 query= "Insert into "+ia.get(0)+" (username,password,name,Role,phone,email,birth_date,joinnig_date,Adress) values('"
-                    +ia.get(1)+"','"+ia.get(2)+"','"+ia.get(3)+"','"+ia.get(4)+"','"+ia.get(5)+"','"+ia.get(6)+"','"+ia.get(7)
-                    +"',CURRENT_DATE,'"+ia.get(9)+"');";
-            }
-            else if(ia.get(0)=="event_mgr"){
-                 query= "Insert into "+ia.get(0)+" (username,password,name,phone,email,gender,birth_date,joinnig_date,Adress) values('"
-                    +ia.get(1)+"','"+ia.get(2)+"','"+ia.get(3)+"','"+ia.get(4)+"','"+ia.get(5)+"','"+ia.get(6)+"','"+ia.get(7)
-                    +"',CURRENT_DATE,'"+ia.get(9)+"');";
-            }
-            else if(ia.get(0)=="physio"){
-                 query= "Insert into "+ia.get(0)+" (username,password,name,phone,email,gender,birth_date,joinnig_date,Adress) values('"
-                    +ia.get(1)+"','"+ia.get(2)+"','"+ia.get(3)+"','"+ia.get(4)+"','"+ia.get(5)+"','"+ia.get(6)+"','"+ia.get(7)
-                    +"',CURRENT_DATE,'"+ia.get(9)+"');";
-            }
-            
-            
-            
-            
-            st.executeUpdate(query); 
-            System.out.println(query);
-            JOptionPane.showMessageDialog(null, "Registration successfull!");
-        }
-        catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Admin databse exception!"+e);
-            System.out.println(e);
-            e.printStackTrace();
-        }
-        
-    }
-    public void update(ArrayList ia){
-        try{
-            String query=null;
-            
-            if(ia.get(0)=="player"){
-                 query= "update "+ia.get(0)+" set password='"+ia.get(2)+"',name='"+ia.get(3)+"',Jersey_no="+ia.get(4)+",gender='"+ia.get(5)+"',"
-                         + "phone='"+ia.get(6)+"',email='"+ia.get(7)+"',Address='"+ia.get(8)+"',birth_date='"+ia.get(9)+"',player_role='"+ia.get(11)+"',bat_style='"+ia.get(12)+"',bowl_style='"+ia.get(13)+"' where username='"+ia.get(1)+"';";
-            }
-            else if(ia.get(0)=="coach"){
-                 query= "update "+ia.get(0)+" set password='"+ia.get(2)+"',name='"+ia.get(3)+"',Role='"+ia.get(4)+"',phone='"+ia.get(5)+"',"
-                         + "email='"+ia.get(6)+"',birth_date='"+ia.get(7)+"',"
-                         + "Adress='"+ia.get(9)+"' where username='"+ia.get(1)+"';";
-            }
-            else if(ia.get(0)=="event_mgr"){
-                 query="update "+ia.get(0)+" set password='"+ia.get(2)+"',name='"+ia.get(3)+"',phone='"+ia.get(4)+"',"
-                         + "email='"+ia.get(5)+"', gender='"+ia.get(6)+"',birth_date='"+ia.get(7)+"',"
-                         + "Adress='"+ia.get(9)+"' where username='"+ia.get(1)+"';";
-            }
-            else if(ia.get(0)=="physio"){
-                 query="update "+ia.get(0)+" set password='"+ia.get(2)+"',name='"+ia.get(3)+"',phone='"+ia.get(4)+"',"
-                         + "email='"+ia.get(5)+"', gender='"+ia.get(6)+"',birth_date='"+ia.get(7)+"',"
-                         + "Adress='"+ia.get(9)+"' where username='"+ia.get(1)+"';";
-            }
-            
-            
-            
-            
-            st.executeUpdate(query);
-            System.out.println(query);
-            JOptionPane.showMessageDialog(null, "Updated successfully!");
-        }
-        catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Databse exception!"+e);
-            System.out.println(e);
-            e.printStackTrace();
-        }
-        
-    }
-    public ResultSet getSearchRes(ArrayList alist){
-        String query;
-        try{
-            query="select * from `"+alist.get(0)+"` where username='"+alist.get(1)+"';";
-            rs=st.executeQuery(query);
-            System.out.println(query);
-            return rs;
-        }
-        catch(Exception e){
-            System.out.println("Error: "+e);
-            e.printStackTrace();
-        }
-        return null;
-    }
-    public ResultSet getLoadRes(ArrayList alist){
-        try{
-            String query = "select * from `"+alist.get(0)+"`;"; 
-            rs=st.executeQuery(query);
-            System.out.println(query);
-            return rs;
-        }
-        catch(SQLException e){
-            System.out.println(e);
-            e.printStackTrace();
-        }
-        
-        return null;
-    }
-    public void DeleteProfile(ArrayList alist){
-         try{
-            String query = "delete from `"+alist.get(0)+"` where username='"+alist.get(1)+"';"; 
-            st.executeUpdate(query);
-            System.out.println(query);
-            JOptionPane.showMessageDialog(null, "Profile Deleted successfully!");
-            
-        }
-        catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Databse exception!"+e);
-            System.out.println(e);
-            e.printStackTrace();
-            
-        }
-        
-        
-    }
+
     void practice_session()
     {
         // practice sessions
@@ -207,6 +70,25 @@ public ResultSet getInfo(){
         float m_salary;
         String schedule;
 
+    manager((String mid, String f, String l, int age, String contact, String address, float sal,String schedule) 
+    {
+        mid = pid;
+        m_fname = f;
+        m_lname = l;
+        m_age = age;
+        m_contact = contact;
+        m_address = address;
+        m_salary = sal;
+        schedule = schedule;
+    }
+
+        //salary of the managers
+        public void calculateNetSalary (double pfPercentage){
+        double pfAmount = m_salary * (pfPercentage / 100);
+        double netSalary = m_salary - pfAmount;
+        this.setNetSalary(netSalary);
+    }
+        }
         public void schedeuleEvent(ArrayList ia){
         try{
             String query="insert into event ( date,venue,team1,team2) values('"+ia.get(0)+" "+ia.get(1)+"','"+ia.get(2)+"','"+ia.get(3)+"','"+ia.get(4)+"');";
@@ -252,18 +134,7 @@ public ResultSet getInfo(){
         }
         return null;
     }
-        manager()
-        {
-            //initialise the data 
-        }
-         void display()
-         {
-             //display the employee details 
-         }
-        void salary()
-        {
-            //salary of the employees
-        }
+    
 
         void team()
         {
